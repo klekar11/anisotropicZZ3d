c44c366a0f0c29962b6c74791a30cd25cbf1c9a2
klekar11 <klekar11@gmail.com>
2026-02-27 18:13:51 +0100

    Added .gitignore and fixed error metrics to interpolate from higher space

--- error_metrics.py
+++ error_metrics.py
@@ -6,20 +6,34 @@ from dolfinx.fem import Function, functionspace, form
 from dolfinx.fem.petsc import assemble_vector
 from petsc4py import PETSc
 
-def compute_error_metrics(domain, V, uh, u_ex, eta):
-    
-    e = Function(V)
-    e.x.array[:] = u_ex.x.array[:] - uh.x.array[:]
+def compute_error_metrics(uh, u_numpy, eta, degree_raise=3):
+    domain   = uh.function_space.mesh
+    family   = uh.function_space.ufl_element().family_name
+    degree   = uh.function_space.ufl_element().degree
 
-    norm_grad_e = np.sqrt(
-        fem.assemble_scalar(form(ufl.inner(ufl.grad(e), ufl.grad(e)) * ufl.dx))
-    )
-    norm_grad_u = np.sqrt(
-        fem.assemble_scalar(form(ufl.inner(ufl.grad(u_ex), ufl.grad(u_ex)) * ufl.dx))
-    )
-    norm_grad_uh = np.sqrt(
-        fem.assemble_scalar(form(ufl.inner(ufl.grad(uh), ufl.grad(uh)) * ufl.dx))
-    )
+    W = functionspace(domain, (family, degree + degree_raise))
+
+    uh_W = Function(W)
+    uh_W.interpolate(uh)
+
+    u_ex_W = Function(W)
+    u_ex_W.interpolate(u_numpy)
+
+    e_W = Function(W)
+    e_W.x.array[:] = u_ex_W.x.array[:] - uh_W.x.array[:]
+
+    norm_grad_e = np.sqrt(domain.comm.allreduce(
+        fem.assemble_scalar(form(ufl.inner(ufl.grad(e_W), ufl.grad(e_W)) * ufl.dx)),
+        op=MPI.SUM
+    ))
+    norm_grad_u = np.sqrt(domain.comm.allreduce(
+        fem.assemble_scalar(form(ufl.inner(ufl.grad(u_ex_W), ufl.grad(u_ex_W)) * ufl.dx)),
+        op=MPI.SUM
+    ))
+    norm_grad_uh = np.sqrt(domain.comm.allreduce(
+        fem.assemble_scalar(form(ufl.inner(ufl.grad(uh_W), ufl.grad(uh_W)) * ufl.dx)),
+        op=MPI.SUM
+    ))
 
     TRE = norm_grad_e  / norm_grad_u
     ERE = eta          / norm_grad_uh
